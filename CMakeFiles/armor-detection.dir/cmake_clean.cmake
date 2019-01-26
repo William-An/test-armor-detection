2fff108674301563b0987bc3df95edf88cbe2c72
William-An <China_Aisa@live.com>
2019-01-26 15:13:14 -0500

    Initial commit

---
+++ CMakeFiles/armor-detection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/armor-detection.dir/armor-detection.cpp.o"
+  "CMakeFiles/armor-detection.dir/Settings.cpp.o"
+  "armor-detection.pdb"
+  "armor-detection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/armor-detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
